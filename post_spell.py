988bd8524be90665d3fbfed5c6ae94a613c1fade
azokal <azokal@sd-27903.dedibox.fr>
2011-10-08 14:37:44 +0200

    fucking wifi

--- post_spell.py
+++ post_spell.py
@@ -18,4 +18,17 @@ def post_spell(name=None):
             g.db.close()
             return render_template('post_spell.html', name=name, entries=entries, len=len(entries), i = 0)
     else:
-        return render_template('post_spell.html', name = name)
+        if (request.method == 'POST'):
+            g.db = connect_db(app.config['USER_DB'])
+            g.db.execute('insert into hero (nam, typ, des, bio, str_start, agi_start, int_start, str_lvl, agi_lvl, int_lvl, life, mana, damages, range, cast_speed, anim_speed, vision, armor, asped, ms) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                         [request.form['nam'], request.form['typ'], request.form['des'],
+                          request.form['bio'], request.form['str_start'],
+                          request.form['agi_start'], request.form['int_start'],
+                          request.form['str_lvl'], request.form['agi_lvl'],
+                          request.form['int_lvl'], request.form['life'],
+                          request.form['mana'], request.form['damages'], request.form['range'],
+                          request.form['cast_speed'], request.form['anim_speed'],
+                          request.form['vision'], request.form['armor'], request.form['aspeed'],
+                          request.form['ms']])
+            g.db.close()
+        return render_template('post_spell.html', name=name)
