ea3ae201788387ed33a4eae308eb54f05e7b5bed
maxime constantinian <azokal@azokal-laptop.(none)>
2011-10-15 00:03:33 +0200

    SPELL FINI YOUPI git statusgit status

--- hero.py
+++ hero.py
@@ -24,7 +24,17 @@ def hero(name=None):
                         ms=row[20]) for row in cur.fetchall()]
         if len(entries) == 0:
             return redirect(url_for('hero'))
-        cur = g.db.execute('select * from spells where name_hero = ?', [entries[0]['name']])
-        spells = [dict(id_spell=[0], name=row[1]) for row in cur.fetchall()]
+        cur = g.db.execute('select * from spells where name_hero = ? order by pos asc', [entries[0]['name']])
+        spells = [dict(id_spell=row[0], name_hero=row[1], name=row[2],
+                       des=row[3], abi_type=row[4], allo_tar=row[5],
+                       pos=row[6]) for row in cur.fetchall()]
+        spell_des = []
+        for result in spells:
+            cur = g.db.execute('select * from spells_effect where id_spell = ? order by lvl_spell asc', [result['id_spell']])
+            for row in cur.fetchall():
+                spell_des.append([dict(id_spell=row[1], lvl_spell=row[2],
+                                       cd=row[3], rang=row[4],
+                                       mana_cost=row[5], life_cost=row[6],
+                                       aoe=row[7], effect=row[8])])
         g.db.close()
-        return render_template('hero.html', entries = entries, spells = spells)
+        return render_template('hero.html', entries=entries, spells=spells, spell_des=spell_des)
