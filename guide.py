692b142f8ef3f04246ade56ca40b7862bab8fb9d
Louis Roché <louis@louisroche.net>
2011-11-09 21:29:45 +0100

    guides sans le remplacement des éléments par le contenu

--- guide.py
+++ guide.py
@@ -2,6 +2,7 @@ import sqlite3
 import re
 from flask import render_template, g, url_for, redirect, Markup
 from markdown import markdown
+from post_guide import parse_balise
 from dota2 import app
 
 def connect_db(base):
@@ -19,5 +20,5 @@ def guide(id=None):
         guides = [dict(id=row[0], titre=row[1], hero=row[2]) for row in cur.fetchall()]
     else:
         cur = g.db.execute('select title, hero, tag, difficulties, content_markup, autor from guide where id = %i' % id)
-        content = [dict(titre=row[0], hero=row[1], tag=row[2], difficulte=row[3], body=Markup(row[4]), auteur=row[5])for row in cur.fetchall()][0]
+        content = [dict(titre=row[0], hero=row[1], tag=row[2], difficulte=row[3], body=Markup(parse_balise(row[4])), auteur=row[5])for row in cur.fetchall()][0]
     return render_template('guide.html', error=error, content=content, guides=guides, id=id)
--- post_guide.py
+++ post_guide.py
@@ -9,13 +9,120 @@ from datetime import datetime
 def connect_db(base):
     return sqlite3.connect(base)
 
-def parse_balise(content):
-    tbl = re.compile(r'\[lvl=[0-9,]*\]')
-    item = re.compile(r'\[item=[a-zA-Z ]*\]')
+def parse_tbl(content):
+    g.db = connect_db(app.config['USER_DB'])
+    tbl = re.compile(r'\[lvl=[1-5,]*\]')
+    tableaux = tbl.findall(content)
+    contenttmp = tbl.split(content)
+    i = 0
+    guide = ""
+    for tab in tableaux:
+        guide += contenttmp[i]
+        i += 1
+        guide += "<table border><caption>Sorts</caption>"
+        tab = re.findall(r'[1-5]', tab)
+        for ligne in range(5):
+            guide += "<tr><td>sort %i</td>" % (ligne + 1)
+            for lvl in range(len(tab)):
+                guide += "<td>"
+                if ligne + 1 == int(tab[lvl]):
+                    guide += "X"
+                else:
+                    guide += " "
+                guide += "</td>"
+            guide += "</tr>"
+        guide += "</table>"
+    guide += contenttmp[i]
+    return (guide)
+
+def parse_hero(content):
     hero = re.compile(r'\[hero=[a-zA-Z ]*\]')
+    g.db = connect_db(app.config['USER_DB'])
+    heros = hero.findall(content)
+    contenttmp = hero.split(content)
+    guide = ""
+    i = 0
+    for hro in heros:
+        guide += contenttmp[i]
+        i += 1
+        hroname = re.search(r'=[a-zA-Z]*', hro).group(0)[1:]
+        cur = g.db.execute('select * from hero where nam like ?', [hroname])
+        hroinfos = [dict(name=row[1]) for row in cur.fetchall()]
+        if len(hroinfos) != 0:
+            guide += hroname
+        else:
+            guide += "erreur : %s" % hro
+    guide += contenttmp[i]
+    return guide
+
+
+def parse_spell(content):
+    g.db = connect_db(app.config['USER_DB'])
     spell = re.compile(r'\[spell=[a-zA-Z]*\]')
+    spells = spell.findall(content)
+    contenttmp = spell.split(content)
+    guide = ""
+    i = 0
+    for spl in spells:
+        guide += contenttmp[i]
+        i += 1
+        splname = re.search(r'=[a-zA-Z]*', spl).group(0)[1:]
+        cur = g.db.execute('select id, des from spells where nam like ?', [splname])
+        entries = [dict(id=row[0], desc=row[1]) for row in cur.fetchall()]
+        if len(entries) != 0:
+            entries = entries[0]
+            cur = g.db.execute("select cd, mana_cost, life_cost, effect from spells_effect where lvl_spell = ? and id = ?", [4, int(entries['id'])])
+            effects = [dict(cd=row[0], mana=row[1], life=row[2], effect=row[3]) for row in cur.fetchall()][0]
+            guide += splname
+        else:
+            guide += "erreur : %s" % spl
+    guide += contenttmp[i]
+    return guide
+
+
+def parse_item(content):
+    g.db = connect_db(app.config['USER_DB'])
+    item = re.compile(r'\[item=[a-zA-Z ]*\]')
+    items = item.findall(content)
+    contenttmp = item.split(content)
+    guide = ""
+    i = 0
+    for itm in items:
+        guide += contenttmp[i]
+        i += 1
+        itmname = re.search(r'=[a-zA-Z]*', itm).group(0)[1:]
+        cur = g.db.execute('select * from items where nam like ?', [itmname])
+        itminfos = [dict(name=row[1]) for row in cur.fetchall()]
+        if len(itminfos) != 0:
+            guide += itmname
+        else:
+            guide += "erreur : %s" % itm
+    guide += contenttmp[i]
+    return guide
+
+def parse_balise(content):
+    content = parse_tbl(content)
+    content = parse_item(content)
+    content = parse_hero(content)
+    content = parse_spell(content)
     return content
 
+
+def valid_guide():
+    if (len(request.form['titre']) == 0 or
+        len(request.form['content']) == 0 or
+        len(request.form['difficulte']) == 0):
+        error = "Element manquant"
+        hid = request.form['hero']
+        titre=request.form['titre']
+        content=request.form['content']
+        tag=request.form['tag']
+        diff = request.form['difficulte']
+        return render_template('post_guide.html',
+                               hero=heros, herolen=herolen, hid=hid, # heros liste
+                               titre=titre, tag=tag, content=content, diff=diff)
+    else:
+        return True
 
 def add_guide():
     g.db = connect_db(app.config['USER_DB'])
@@ -37,7 +144,7 @@ def update_guide(id_guide):
                  [request.form['titre'],
                   request.form['tag'],
                   request.form['content'],
-                  markdown(Markup.escape(request.form['guide'])),
+                  parse_balise(markdown(Markup.escape(request.form['guide']))),
                   datetime.today(), id_guide])
     g.db.commit()
 
@@ -69,10 +176,11 @@ def post_guide(id_guide=None):
                     tag=request.form['tag']
                     diff = request.form['difficulte']
                     return render_template('post_guide.html',
-                                           previsualisation=1, guide=Markup(markdown(guide)), # previsualisation
-                                           hero=heros, herolen=herolen, # heros liste
+                                           previsualisation=1, guide=Markup(parse_balise(markdown(guide))), # previsualisation
+                                           hero=heros, herolen=herolen, hid=hid,# heros liste
                                            titre=titre, tag=tag, content=content, diff=diff)
                 else:
+                    valid_guide()
                     if (id_guide == None):
                         id_guide = add_guide()
                     else:
@@ -98,5 +206,5 @@ def post_guide(id_guide=None):
                                                titre=titre, tag=tag, content=guide, diff=diff)
                     else:
                         return render_template('post_guide.html', hero=heros, herolen=herolen)
-    return redirect(url_for('default'))
+    return redirect(url_for('guide'))
 
