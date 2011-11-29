9a67d7250d3d93c35f1a501320c3faf8eb5a02d0
maxime constantinian <consta_m@Azokal-Laptop.localdomain>
2011-11-29 01:50:26 +0100

    correction d une erreur sur les news

--- news.py
+++ news.py
@@ -43,9 +43,9 @@ def news(id_news=None, page=None):
         cur = g.db.execute('select Count(*) from news')
         count_page = [dict(nb=row[0]) for row in cur.fetchall()]
         pagemax = count_page[0]['nb'] / 10 + 1
-        print pagemax
         if (page >= pagemax):
             page = pagemax
+        print (page * 10) - 10
         cur = g.db.execute('select * from news order by id desc limit ?, 10',[(page * 10) - 10])
         entries = []
         for row in cur.fetchall():
@@ -53,9 +53,9 @@ def news(id_news=None, page=None):
                    'tag': row[3], 'news': Markup(row[5]), 'date': row[6]}
             date = datetime.strptime(dic['date'], "%Y-%m-%d %H:%M:%S.%f")
             dic['date'] = date.strftime(u"%d %B %Y à %H:%M:%S".encode('utf-8')).decode('utf-8')
-            tmp = g.db.execute("SELECT Count(*) FROM commentaire where genre like 'news' and id_genre = ?", str(row[0]));
+            tmp = g.db.execute("SELECT Count(*) FROM commentaire where genre like 'news' and id_genre = ?", [row[0]]);
             commentaire = [dict(nb=row[0]) for row in tmp.fetchall()]
-            print commentaire[0]['nb']
+            #print commentaire[0]['nb']
             dic['nb_com'] = commentaire[0]['nb']
             entries.append(dic)
         return render_template('news.html', entries=entries, page=int(page), pagemax=int(pagemax))
